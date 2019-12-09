89c6059f6ff1d7d5cb415974264d382dbe87d15c
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-09 18:22:41 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 
 def say_hello(name, language = "Ruby")
-  puts "Hello #{name}"
+  puts "Hello #{name}, I hear your a great #{language} programmer."
   
   
 end
